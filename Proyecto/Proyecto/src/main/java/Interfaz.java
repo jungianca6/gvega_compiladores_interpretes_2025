c0ddc33dc15adbeb55d257428f29d50dc71c9b5e
Giancarlo Vega Marin <jungianca6@estudiantec.cr>
2025-11-16 15:37:06 -0600

    interfaz y componentes
    le faltan las funciones

---
+++ Proyecto/Proyecto/src/main/java/Interfaz.java
@@ -0,0 +1,75 @@
+import javax.swing.*;
+import java.awt.*;
+
+class CompiInterfaz extends JFrame {
+
+    private JPanel panel;
+    private JLabel etiqueta;
+    private JTextField chatTexto;
+    private JButton boton;
+    private JTextArea codigo;
+
+    public CompiInterfaz(){
+        this.setBounds(500,200,600,600);
+        setTitle("Interfaz");
+
+        ComponentesCliente();
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+    private void ComponentesCliente(){
+        panelGUI();
+        colocarAreaCodigo();
+        etiquetaGUI();
+        //colocarCajadeTexto();
+        colocarBoton();
+    }
+
+    private void panelGUI(){
+        panel = new JPanel();
+        panel.setLayout(null);
+        panel.setBackground(Color.pink);
+        this.getContentPane().add(panel);
+    }
+
+    private void etiquetaGUI(){
+        etiqueta = new JLabel("Interfaz del compilador",SwingConstants.CENTER);
+        panel.add(etiqueta);
+        etiqueta.setBounds(200,20,200,25);
+        etiqueta.setForeground(Color.WHITE);
+        etiqueta.setBackground(Color.BLACK);
+        etiqueta.setFont(new Font("times new roman", Font.PLAIN,20));
+        etiqueta.setOpaque(true);
+    }
+
+    private void colocarAreaCodigo(){
+        codigo = new JTextArea();
+        codigo.setBounds(50,100,200,350);
+        codigo.setText("Escribe el codigo aqui");
+        panel.add(codigo);
+        codigo.setEditable(true);
+        codigo.setLineWrap(true);
+
+
+        JScrollPane scroll = new JScrollPane(codigo,
+                JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scroll.setBounds(50,100,200,340);
+        panel.add(scroll);
+    }
+
+    private void colocarBoton(){
+        boton = new JButton("Subir código");
+        boton.setBounds(300,390,160,30);
+        panel.add(boton);
+        boton.setEnabled(true);
+    }
+
+}
+
+public class Interfaz {
+    public static void main(String[] args) {
+        CompiInterfaz compiGUI = new CompiInterfaz();
+        compiGUI.setVisible(true);
+        compiGUI.setResizable(false);
+    }
+}
